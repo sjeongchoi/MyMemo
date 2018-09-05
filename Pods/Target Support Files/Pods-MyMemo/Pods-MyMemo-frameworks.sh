6c29f7dfec1910110d1f1d57f24001df02e89d7b
mabel <tandri6@gmail.com>
2018-09-05 13:51:01 +0900

    Add EmptyDataSet_Swift for empty table view

--- Pods/Target Support Files/Pods-MyMemo/Pods-MyMemo-frameworks.sh
+++ Pods/Target Support Files/Pods-MyMemo/Pods-MyMemo-frameworks.sh
@@ -143,9 +143,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/EmptyDataSet-Swift/EmptyDataSet_Swift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FontBlaster/FontBlaster.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/EmptyDataSet-Swift/EmptyDataSet_Swift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/FontBlaster/FontBlaster.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
